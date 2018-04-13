528d211397e989126e8e337721b6f5cf10a88920
jk722 <jk722@wins21.co.rk>
2018-04-13 10:24:00 +0900

    share memory 기본 exmaple 추가

--- memory_study/share_memory.c
+++ memory_study/share_memory.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
 #include <string.h> 
 #include <unistd.h> 
-
+#include "share_memory.h"
 
 int main()
 {
 	int shmid;
-	int pid;
-
-	int *cal_num;
+	int i;
+	SHM_INFOS *shm_info= NULL;
 	void *shared_memory = (void *)0;
 
 
 	// 공유메모리 공간을 만든다.
-	shmid = shmget((key_t)1234, sizeof(int), 0666|IPC_CREAT);
+	shmid = shmget((key_t)3836, sizeof(SHM_INFOS)*SHM_INFO_COUNT, 0666|IPC_CREAT);
 
 	if (shmid == -1)
 	{
@@ -30,40 +31,14 @@ int main()
 		exit(0);
 	}
 
-	cal_num = (int *)shared_memory;
-	pid = fork();
-	if (pid == 0)
-	{
-		shmid = shmget((key_t)1234, sizeof(int), 0);
-		if (shmid == -1)
-		{
-			perror("shmget failed : ");
-			exit(0);
-		}
-		shared_memory = shmat(shmid, (void *)0, 0666|IPC_CREAT);
-		if (shared_memory == (void *)-1)
-		{
-			perror("shmat failed : ");
-			exit(0);
-		}
-		cal_num = (int *)shared_memory;
-		*cal_num = 1;
+	shm_info = (SHM_INFOS *)shared_memory;
 
-		while(1)
-		{
-			*cal_num = *cal_num + 1;
-			printf("child %d\n", *cal_num); 
-			sleep(1);
-		}
-	}
-
-	// 부모 프로세스로 공유메모리의 내용을 보여준다. 
-	else if(pid > 0)
+	while(1)
 	{
-		while(1)
-		{
-			sleep(1);
-			printf("%d\n", *cal_num);
+		for(i=0 ;i < SHM_INFO_COUNT; i++){
+			snprintf(shm_info[i].str_ip,sizeof(shm_info[i].str_ip),"1.1.1.%d",i);
+			shm_info[i].int_ip =  12891010 +i;
+			shm_info[i].int_id =  128 + i;
 		}
 	}
 }
